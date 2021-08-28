9df68396d711310ab6ef4944749e7fe55c633a6c
Ekene Ejeh <ekeneejeh@gmail.com>
2021-08-28 20:38:38 +0100

    Finished_the_app

--- js/main.js
+++ js/main.js
@@ -1,52 +1,19 @@
-// // // alert("Hello World");
-// // let num = (document.getElementById("count-el").innerHTML = 20);
-
-// let myCurrentAge = 32;
-// console.log(myCurrentAge);
-
-// let myAge = 32;
-// let humanDogRatio = 7;
-
-// let myDogAge = myAge * humanDogRatio;
-// console.log(myDogAge);
-
-// function increment() {
-//    console.log("Button was Clicked!");
-// }
-
-// let Lap1 = 34;
-// let Lap2 = 33;
-// let Lap3 = 36;
-
-// function totalLapTime() {
-//    let total = Lap1 + Lap2 + Lap3;
-//    console.log(total);
-// }
-// totalLapTime();
-
-// let lapsCompleted = 0;
-
-// function incrementLap() {
-//    // let increment = lapsCompleted + 1;
-//    for (let index = 0; index <= 3; index++) {
-//       const element = lapsCompleted + index;
-//       console.log(element);
-//    }
-// }
-// incrementLap();
+// variable used
+let count = 0;
+let currentCount = document.getElementById("count-el");
+let saveCount = document.getElementById("save-el");
 
 // increment count when the increment button is clicked
-let count = 0;
-// let newCount;
-let currentCount = document.getElementById("count-el");
-let saveCount = document.getElementById("count-save");
-
 function increment() {
    Count = count++;
-   currentCount.innerHTML = Count + 1;
+   currentCount.innerText = Count + 1;
 }
 
+// save count and start a new when save button is clicked
 function save() {
    console.log(Count + 1);
-   // saveCount.innerHTML = `We counted ${Count} People`;
+   let countResult = `${count}, `;
+   saveCount.textContent += `${countResult}`;
+   currentCount.textContent = 0;
+   count = 0;
 }
