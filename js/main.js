7c851649e0db37f57af0ecfe3d1957fada3de048
Ekene Ejeh <ekeneejeh@gmail.com>
2021-08-28 12:34:35 +0100

    added_both_Increment_and_save_button-increment_button_is_working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,52 @@
-// // alert("Hello World");
-// let num = (document.getElementById("count-el").innerHTML = 20);
+// // // alert("Hello World");
+// // let num = (document.getElementById("count-el").innerHTML = 20);
 
-let myAge = 32;
-console.log(myAge);
+// let myCurrentAge = 32;
+// console.log(myCurrentAge);
+
+// let myAge = 32;
+// let humanDogRatio = 7;
+
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+// function increment() {
+//    console.log("Button was Clicked!");
+// }
+
+// let Lap1 = 34;
+// let Lap2 = 33;
+// let Lap3 = 36;
+
+// function totalLapTime() {
+//    let total = Lap1 + Lap2 + Lap3;
+//    console.log(total);
+// }
+// totalLapTime();
+
+// let lapsCompleted = 0;
+
+// function incrementLap() {
+//    // let increment = lapsCompleted + 1;
+//    for (let index = 0; index <= 3; index++) {
+//       const element = lapsCompleted + index;
+//       console.log(element);
+//    }
+// }
+// incrementLap();
+
+// increment count when the increment button is clicked
+let count = 0;
+// let newCount;
+let currentCount = document.getElementById("count-el");
+let saveCount = document.getElementById("count-save");
+
+function increment() {
+   Count = count++;
+   currentCount.innerHTML = Count + 1;
+}
+
+function save() {
+   console.log(Count + 1);
+   // saveCount.innerHTML = `We counted ${Count} People`;
+}
